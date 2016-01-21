d02c0dc56e5c58791d416be03ccbe0317c4c5573
JanPetterMG <europe.jpg@gmail.com>
2016-01-21 23:27:21 +0100

    Some more code, still WIP

--- source/CleanParamFilter.php
+++ source/CleanParamFilter.php
@@ -19,253 +19,293 @@ use vipnytt\CleanParamFilter\extensions;
 
 class CleanParamFilter
 {
-	// Approved URLs
-	protected $approved = array();
+    // CleanParam array types
+    const PARAM_URL = 'param=>url';
+    const URL_PARAM = 'url=>param';
 
-	// Duplicate URLs
-	protected $duplicate = array();
+    // Approved URLs
+    protected $approved = array();
 
-	// Status
-	private $filtered = false;
+    // Duplicate URLs
+    protected $duplicate = array();
 
-	// Clean-Param set
-	private $cleanParam = array();
+    // Status
+    private $filtered = false;
 
-	// URL set
-	private $urls = array();
+    // Clean-Param set
+    private $cleanParam = array();
 
-	// Statistics
-	private $statistics = array();
+    // URL set
+    private $urls = array();
 
-	/**
-	 * Constructor
-	 *
-	 * @param array|string $cleanParam
-	 * @param array|string $urls
-	 */
-	public function __construct($cleanParam, $urls = null)
-	{
-		$this->addCleanParam($cleanParam);
-		$this->addURL($urls);
-	}
+    // Statistics
+    private $statistics = array();
 
-	/**
-	 * Add CleanParam
-	 *
-	 * @param array|string $cleanParam
-	 */
-	public function addCleanParam($cleanParam)
-	{
-		switch (gettype($cleanParam)) {
-			case 'array':
-				$this->addCleanParamArray($cleanParam);
-				break;
-			case 'string':
-				$this->addCleanParam($this->initialize_robots_txt_parser($cleanParam));
-				break;
-		}
-	}
+    /**
+     * Constructor
+     *
+     * @param array|string $cleanParam
+     * @param array|string $urls
+     */
+    public function __construct($cleanParam, $urls = null)
+    {
+        $this->addCleanParam($cleanParam);
+        $this->addURL($urls);
+    }
 
-	/**
-	 * Add Clean-Param array
-	 *
-	 * @param array $array
-	 */
-	private function addCleanParamArray($array)
-	{
-		//$this->cleanParam = array_merge($array, $this->cleanParam);
-		foreach ($array as $key => $value) {
-			if (gettype($key) != 'string' ||
-				gettype($value) != 'array'
-			) {
-				// TODO: trigger error, unknown format
-				return;
-			}
-			// $key = URL
-			// $value = parameter(s)
-			// OR
-			// $key = parameter(s)
-			// $value = URL
-			if (empty($value)) {
-				$value = array('/*');
-			}
-		}
-	}
+    /**
+     * Add CleanParam
+     *
+     * @param array|string $cleanParam
+     */
+    public function addCleanParam($cleanParam)
+    {
+        switch (gettype($cleanParam)) {
+            case 'array':
+                $this->addCleanParamArray($cleanParam);
+                break;
+            case 'string':
+                $this->addCleanParam($this->initialize_robots_txt_parser($cleanParam));
+                break;
+        }
+    }
 
-	public static function getParserExtensions()
-	{
-		$extensions = array();
-		$files = glob(dirname(__FILE__) . '/*.inc.php', GLOB_BRACE);
-		if ($files !== false) {
-			foreach ($files as $file) {
-				include_once($file);
-				$class = rtrim($file, ".inc.php");
-				if (method_exists($class, 'isInstalled')
-					&& is_callable(array($class, 'isInstalled'))
-					&& $class->isInstalled()
-				) {
-					$extensions[] = $class;
-				}
-			}
-		}
-		return $extensions;
-	}
+    /**
+     * Add Clean-Param array
+     *
+     * @param array $array
+     */
+    private function addCleanParamArray($array)
+    {
+        //TODO: Add support for parsing of basic CleanParam strings, in case of one in each array item
+        foreach ($array as $key => $value) {
+            if (gettype($key) != 'string' ||
+                gettype($value) != 'array'
+            ) {
+                // TODO: trigger error, unknown format
+                return;
+            }
+            $type = $this->detectArrayType($array);
+            if (empty($value)) {
+                $value = array('/*');
+            }
+        }
+    }
 
-	/**
-	 * Robots.txt parser
-	 *
-	 * @param string $content - robots.txt valid content
-	 * @return array - [URL]=param or [param]=URL
-	 */
-	private function initialize_robots_txt_parser($content)
-	{
-		$extensions = $this->getParserExtensions();
-		foreach ($extensions as $parser) {
-			$cleanParam = new $parser->parse($content);
-			if (is_array($cleanParam)) {
-				return $cleanParam;
-			}
-		}
-		trigger_error("Unable to parse Clean-Param string, no supported robots.txt parsers installed, please use pre-parsed array instead", E_USER_WARNING);
-		return array();
-	}
+    /**
+     * Detect CleanParam array type
+     *
+     * @param array $array - CleanParam array
+     * @return string - array type
+     */
+    private function detectArrayType($array)
+    {
+        foreach ($array as $lvl1 => $sub) {
+            if (stripos($lvl1, '/') !== false) {
+                return self::URL_PARAM;
+            } elseif (stripos($lvl1, '&') !== false) {
+                return self::PARAM_URL;
+            }
+            foreach ($sub as $lvl2) {
+                if (stripos($lvl2, '/') !== false) {
+                    return self::URL_PARAM;
+                } elseif (stripos($lvl2, '&') !== false) {
+                    return self::PARAM_URL;
+                }
+            }
+        }
+    }
 
-	/**
-	 * Add URL(s)
-	 *
-	 * @param array|string $url
-	 * @return void
-	 */
-	public function addURL($url)
-	{
-		switch (gettype($url)) {
-			case 'array':
-				$this->parseURLArray($url);
-				break;
-			case 'string':
-				$this->prepareURL($url);
-				break;
-		}
-	}
+    /**
+     * Reformat parameters
+     *
+     * @param string $parameters - unparsed parameters
+     * @return array - individual parameters
+     */
+    protected function parseParameters($parameters)
+    {
+        return explode('&', $parameters);
+    }
 
-	/**
-	 * Parse URL array
-	 *
-	 * @param array $array
-	 */
-	private function parseURLArray($array)
-	{
-		foreach ($array as $url) {
-			$this->addURL($url);
-		}
-	}
+    /**
+     * Get a list of robots.txt parser extensions
+     *
+     * @return array
+     */
+    public static function getParserExtensions()
+    {
+        $extensions = array();
+        $files = glob(dirname(__FILE__) . '/*.inc.php', GLOB_BRACE);
+        if ($files !== false) {
+            foreach ($files as $file) {
+                include_once($file);
+                $class = rtrim($file, ".inc.php");
+                if (method_exists($class, 'isInstalled')
+                    && is_callable(array($class, 'isInstalled'))
+                    && $class->isInstalled()
+                ) {
+                    $extensions[] = $class;
+                }
+            }
+        }
+        return $extensions;
+    }
 
-	/**
-	 * Prepare and add URL
-	 *
-	 * @param string $url
-	 */
-	private function prepareURL($url)
-	{
-		$relative = $this->parseURL($url);
-		if ($relative === false) {
-			return;
-		}
-		$this->urls[] = $relative;
-		$this->filtered = false;
-	}
+    /**
+     * Robots.txt parser
+     *
+     * @param string $content - robots.txt valid content
+     * @return array - [URL]=param or [param]=URL
+     */
+    private function initialize_robots_txt_parser($content)
+    {
+        $extensions = $this->getParserExtensions();
+        foreach ($extensions as $parser) {
+            $cleanParam = new $parser->parse($content);
+            if (is_array($cleanParam)) {
+                return $cleanParam;
+            }
+        }
+        trigger_error("Unable to parse Clean-Param string, no supported robots.txt parsers installed, please use pre-parsed array instead", E_USER_WARNING);
+        return array();
+    }
 
-	/**
-	 * Parse single URL
-	 *
-	 * @param  string $url
-	 * @return string|false
-	 */
-	private function parseURL($url)
-	{
-		$parsed = parse_url($url);
-		if ($parsed === false ||
-			!isset($parsed['path'])
-		) {
-			return false;
-		}
-		return $parsed['path'] . (isset($parsed['query']) ? '?' . $parsed['query'] : '');
-	}
+    /**
+     * Add URL(s)
+     *
+     * @param array|string $url
+     * @return void
+     */
+    public function addURL($url)
+    {
+        switch (gettype($url)) {
+            case 'array':
+                $this->parseURLArray($url);
+                break;
+            case 'string':
+                $this->prepareURL($url);
+                break;
+        }
+    }
 
-	/**
-	 * Lists all approved URLs
-	 *
-	 * @return array
-	 */
-	public function listApproved()
-	{
-		$this->filter();
-		return $this->approved;
-	}
+    /**
+     * Parse URL array
+     *
+     * @param array $array
+     */
+    private function parseURLArray($array)
+    {
+        foreach ($array as $url) {
+            $this->addURL($url);
+        }
+    }
 
-	/**
-	 * Filter URLs
-	 *
-	 * @return void
-	 */
-	private function filter()
-	{
-		if ($this->filtered) {
-			return;
-		}
-		$this->optimize();
-		//TODO: Filter
-		$this->filtered = true;
-	}
+    /**
+     * Prepare and add URL
+     *
+     * @param string $url
+     */
+    private function prepareURL($url)
+    {
+        $relative = $this->parseURL($url);
+        if ($relative === false) {
+            return;
+        }
+        $this->urls[] = $relative;
+        $this->filtered = false;
+    }
 
-	/**
-	 * Optimize URL and Clean-Param arrays
-	 *
-	 * @return void
-	 */
-	private function optimize()
-	{
-		$this->cleanParam = array_unique($this->cleanParam);
-		$this->urls = array_unique($this->urls);
-	}
+    /**
+     * Parse single URL
+     *
+     * @param  string $url
+     * @return string|false
+     */
+    private function parseURL($url)
+    {
+        $parsed = parse_url($url);
+        if ($parsed === false ||
+            !isset($parsed['path'])
+        ) {
+            return false;
+        }
+        return $parsed['path'] . (isset($parsed['query']) ? '?' . $parsed['query'] : '');
+    }
 
-	/**
-	 * Lists all duplicate URLs and their replacements
-	 *
-	 * @return array
-	 */
-	public function listDuplicates()
-	{
-		$this->filter();
-		return $this->duplicate;
-	}
+    /**
+     * Lists all approved URLs
+     *
+     * @return array
+     */
+    public function listApproved()
+    {
+        $this->filter();
+        return $this->approved;
+    }
 
-	/**
-	 * Check if URL is duplicate
-	 *
-	 * @param string $url
-	 * @return bool
-	 */
-	public function isDuplicate($url)
-	{
-		$this->filter();
-		$relative = $this->parseURL($url);
-		if ($relative !== false &&
-			isset($this->duplicate[$relative])
-		) {
-			return $this->duplicate[$relative];
-		}
-		return false;
-	}
+    /**
+     * Filter URLs
+     *
+     * @return void
+     */
+    private function filter()
+    {
+        if ($this->filtered) {
+            return;
+        }
+        $this->optimize();
+        //TODO: Filter
+        $this->filtered = true;
+    }
 
-	/**
-	 * Statistics
-	 *
-	 * @return array $count
-	 */
-	public function getStatistics()
-	{
-		$this->filter();
-		return $this->statistics;
-	}
+    /**
+     * Optimize URL and Clean-Param arrays
+     *
+     * @return void
+     */
+    private function optimize()
+    {
+        $this->cleanParam = array_unique($this->cleanParam);
+        $this->urls = array_unique($this->urls);
+    }
+
+    /**
+     * Lists all duplicate URLs and their replacements
+     *
+     * @return array
+     */
+    public function listDuplicates()
+    {
+        $this->filter();
+        return $this->duplicate;
+    }
+
+    /**
+     * Check if URL is duplicate
+     *
+     * @param string $url
+     * @return bool
+     */
+    public function isDuplicate($url)
+    {
+        $this->filter();
+        $relative = $this->parseURL($url);
+        if ($relative !== false &&
+            isset($this->duplicate[$relative])
+        ) {
+            return $this->duplicate[$relative];
+        }
+        return false;
+    }
+
+    /**
+     * Statistics
+     *
+     * @return array $count
+     */
+    public function getStatistics()
+    {
+        $this->filter();
+        return $this->statistics;
+    }
 }
